856b88e85cfecdbf0fa0e4bdb269f9c50c446010
Ansab Gillani <syedansab500@gmail.com>
2026-09-06 16:21:59 +0200

    feat: add KDE CCO package skeleton

---
+++ src/kde_cco/__init__.py
@@ -0,0 +1,3 @@
+"""Chance-constrained optimization with kernel density estimation."""
+
+__version__ = "0.1.0"
---
+++ tests/test_package_import.py
@@ -0,0 +1,3 @@
+def test_package_imports():
+    import kde_cco
+    assert kde_cco.__version__
